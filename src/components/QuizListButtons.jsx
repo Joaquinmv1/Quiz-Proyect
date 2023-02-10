64a68c2731eb717ddff127713ab4d28bce35c498
Joaquinmv1 <villarrealjoaquin09@gmail.com>
2023-02-10 10:53:57 -0300

    cambios en estilos

--- src/components/QuizListButtons.jsx
+++ src/components/QuizListButtons.jsx
@@ -15,13 +15,13 @@ export const QuizListButtons = ({ currentQuestion, newQuestion, time, areDisable
     return (
         <>
             <section className="container-top">
-                <h2>Pregunta<span> {currentQuestion + 1} de {questions.length}</span></h2>
+                <h2 className='h2-top'>Pregunta<span> {currentQuestion + 1} de {questions.length}</span></h2>
             </section>
             <div className="container-center">
                 <div className='question'>{questions[currentQuestion].title}</div>
             </div>
             <div className="container-answers">
-                {time > 3 ? <p style={{ color: 'green', fontWeight: "bold", fontSize:"1.1rem"  }}>Tiempo restante:{time}</p> : <p style={{ color: 'red', fontWeight: "bold", fontSize:"1.1rem"  }}>Tiempo restante:{time}</p>}
+                {time > 5 ? <p style={{ color: 'green', fontWeight: "bold", fontSize:"1.1rem"  }}>Tiempo restante:{time}</p> : <p style={{ color: 'red', fontWeight: "bold", fontSize:"1.1rem"  }}>Tiempo restante:{time}</p>}
                 <div className='answer'>
                     {questions[currentQuestion].options.map((answer, index) => (
                         <QuizButtons key={index} answer={answer} handleClickQuestion={handleClickQuestion} areDisable={areDisable}/>
